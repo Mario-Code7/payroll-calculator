c655f2248d55730612e419e564ea140d2b008b30
Mario-Code7 <marioroa71@yahoo.com>
2025-10-08 15:08:11 -0500

    refactor to add bufferedwriter

--- src/main/java/com/pluralsight/AppEmployee.java
+++ src/main/java/com/pluralsight/AppEmployee.java
@@ -7,59 +7,95 @@ import java.util.regex.Pattern;
 
 public class AppEmployee {
     public static void main(String[] args) {
+        Employee[] employees = readEmployees();
+
+        for (Employee employee: employees) {
+            System.out.println(employee);
+        }
+
+//        try {
+//            // create a FileWriter
+//            FileWriter fileWriter = new FileWriter("payroll.csv", true);
+//            // create a BufferedWriter
+//            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+//            // write to the file
+//            bufferedWriter.write("line 1 is here \n");
+//            bufferedWriter.write("line 2 is here \n");
+//            bufferedWriter.write("line 3 is here \n");
+//
+//            // close the writer
+//            bufferedWriter.close();
+//        } catch (IOException e) {
+//            System.out.println("ERROR: An unexpected error occurred");
+//            e.getStackTrace();
+//        }
+
+
+
+
+
+    }
+
+    private static Employee[] readEmployees() {
+        Employee[] employees = new Employee[8];
+
         try {
-// create a FileReader object connected to the File
+            // create a FileReader object connected to the File
             FileReader fileReader = new FileReader("employees.csv");
-// create a BufferedReader to manage input stream
+            // create a BufferedReader to manage input stream
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
+
             //skip header line
             bufferedReader.readLine();
-// read until there is no more data
+
+            int index = 0;
+            // read until there is no more data
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+//                System.out.println(line);
                 String[] parts = line.split(Pattern.quote("|"));
 
 
                 Employee employee = new Employee();
 
-//                String employeeIdAsString = parts[0];
-//                int employeeId = Integer.parseInt(employeeIdAsString);
-//                employee.setEmployeeId(employeeId);
-//              OR
-//                employee.setEmployeeId(Integer.parseInt(parts[0]));
-                String employeeId = parts[0];
-                employee.setEmployeeId(Integer.parseInt(employeeId));
+                String employeeIdAsString = parts[0];
+                int employeeId = Integer.parseInt(employeeIdAsString);
+                employee.setEmployeeId(employeeId);
+//                OR
+//                  employee.setEmployeeId(Integer.parseInt(parts[0]));
 
                 String name = parts[1];
                 employee.setName(name);
-
-//                String hoursWorkedAsString = parts[2];
-//                double hoursWorked = Double.parseDouble(hoursWorkedAsString);
-//                employee.setHoursWorked(hoursWorked);
-
-                String hoursWorked = parts[2];
-                employee.setHoursWorked(Double.parseDouble(hoursWorked));
-
-//                String payRateAsString = parts[3];
-//                double payRate = Double.parseDouble(payRateAsString);
-//                employee.setPayRate(payRate);
-
-                String payRate = parts[3];
-                employee. setPayRate(Double.parseDouble(payRate));
+//                OR
+//                employee.setName(parts[1]);
 
 
+                String hoursWorkedAsString = parts[2];
+                double hoursWorked = Double.parseDouble(hoursWorkedAsString);
+                employee.setHoursWorked(hoursWorked);
+//                OR
+//                employee.setHoursWorked(Double.parseDouble(parts[2]));
 
 
-                System.out.println(employee);
+                String payRateAsString = parts[3];
+                double payRate = Double.parseDouble(payRateAsString);
+                employee.setPayRate(payRate);
+//                OR
+//                employee.setPayRate(Double.parseDouble(parts[3]));
 
-
+                employees[index] = employee;
+//                System.out.println(employee);
+                index = index + 1; //index++
             }
-// close the stream and release the resources
+            // close the stream and release the resources
             bufferedReader.close();
         } catch (IOException e) {
-// display stack trace if there was an error
+            // display stack trace if there was an error
             e.printStackTrace();
         }
+
+        return employees;
+
+
     }
 }
